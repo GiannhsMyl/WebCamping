48b8cc0d9808a8a8bb1a93ce6b4a8c8709ba67f7
GiannhsMyl <greenspinachkohai@gmail.com>
2025-04-15 17:30:18 +0300

    callendar added to admin Page with Basic Functionality

--- adminPage.js
+++ adminPage.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded",()=>{
 	console.log("main");
+	curCalMonth=(new Date()).getMonth();
+	makeCallendar(curCalMonth);
+	document.querySelector(".callendarLtButton").addEventListener("click",()=>{
+		if(curCalMonth>0)
+			makeCallendar(--curCalMonth);
+	});
+	document.querySelector(".callendarGtButton").addEventListener("click",()=>{
+		if(curCalMonth<11)
+			makeCallendar(++curCalMonth);
+	});
+
+
 	let radio=document.querySelectorAll(".admin-form input[type='radio']");
 	for(let i=0;i<radio.length;i++)
 		radio[i].addEventListener("click",()=>{
@@ -120,4 +132,57 @@ function addReservation(name,zone,people,checkIn,checkOut,toggle=false){
 	row.appendChild(deleteColumn);
 
 	document.querySelector("#reservationList").appendChild(row);
+}
+
+function makeCallendar(month,leapYear=false,year=(new Date()).getFullYear()){//0 JAN 1 FEB 2 MARCH etc
+	let daysInMonth=10;
+	if(month%2===0)//if month even
+		if(month<7)
+			daysInMonth=31;
+		else
+			daysInMonth=30;
+	else//month odd
+		if(month==1)
+			daysInMonth=leapYear? 29:28;
+		else
+			if(month<6)
+				daysInMonth=30;
+			else
+				daysInMonth=31;
+	calBox=document.querySelector(".callendarBox");
+	calHeaderTitle=document.querySelector(".callendarHeader h4");
+	calHeader=document.querySelector(".callendarHeader");
+	calBox.innerHTML="";//resets Callendar
+	// monthText=document.createElement("span");
+	monthText=["ΙΑΝΟΥΑΡΙΟΣ","ΦΕΒΡΟΥΑΡΙΟΣ","ΜΑΡΤΙΟΣ","ΑΠΡΙΛΙΟΣ","ΜΑΙΟΣ","ΙΟΥΝΙΟΣ","ΙΟΥΛΙΟΣ","ΑΥΓΟΥΣΤΟΣ","ΣΕΠΤΕΜΒΡΙΟΣ","ΟΚΤΟΒΡΙΟΣ","ΜΟΕΜΒΡΙΟΣ","¨ΔΕΚΕΜΒΡΙΟΣ"];
+	dayText=["ΚΥΡ","ΔΕΥ","ΤΡΙ","ΤΕΤ","ΠΕΜ","ΠΑΡ","ΣΑΒ"];
+	calHeaderTitle.innerHTML=monthText[month] +" "+ year;
+	for(let i=0;i<7;i++){
+		let temp=document.createElement("span");
+		temp.innerHTML=dayText[i];
+		calBox.appendChild(temp);
+	}
+	let tempDate=(new Date(year,month,1)).getDay();
+	for(let i=1;i<daysInMonth+1+tempDate;i++){
+		let temp=document.createElement("div");
+		if(i>tempDate){
+			temp.setAttribute("data-date",`${year}-${month+1}-${i-tempDate}`);
+			temp.innerHTML=i-tempDate;
+			if(isToday(new Date(`${year}-${month+1}-${i-tempDate}`)))
+				temp.classList.add("today");
+			temp.addEventListener("click",(event)=>{
+				let date=new Date(event.currentTarget.getAttribute("data-Date"));
+				if(temp.classList.contains("today"))
+					document.querySelector("#reservation>h3").innerHTML=`Κρατήσεις Σήμερα`;
+				else
+					document.querySelector("#reservation>h3").innerHTML=`Κρατήσεις ${dayText[date.getDay()]} ${date.getDate()} ${monthText[date.getMonth()]}`;
+				window.location.href = "#reservation";
+			});
+		}
+		calBox.appendChild(temp);
+	}
+}
+function isToday(date){
+	let todayDate=new Date();
+	return date.getDate()===todayDate.getDate() && date.getMonth()===todayDate.getMonth() && date.getFullYear()===todayDate.getFullYear();
 }
