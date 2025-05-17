54eaf8bbfca0ac5c488ad15474e79c4b9ab7f6eb
Panos Dasargyris <panosdasargyris@gmail.com>
2025-05-17 12:39:33 +0300

    Add authentication middlewares

--- routes/router.mjs
+++ routes/router.mjs
@@ -1,19 +1,26 @@
 import express from "express";
 import * as controller from"../controller/controller.mjs"
+import * as authController from '../controller/authController.mjs'
+import { checkAuth } from '../Middleware/authMiddleware.mjs';
 import { rootCertificates } from "tls";
-
 
 const router=express.Router();
 
 router.get("/",controller.mainPage);
 
+router.get("/reservation",controller.reservationPage);
+
 router.get("/contact",controller.contactPage);
 router.post("/new_contact_message",controller.sendContactMessage);
 
-router.get("/reservation",controller.reservationPage);
 
-router.get("/connect",controller.login);
+router.get("/connect", checkAuth, controller.login);
 
 router.get("/admin",controller.adminPage);
+
+router.post("/login", authController.handleLogin);      
+router.get("/logout", authController.handleLogout);   
+
 router.post("/admin/addZone",controller.addZone);
+
 export {router}
